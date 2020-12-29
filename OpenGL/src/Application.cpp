d8810baec20dad5ebdb019d968a254af1f2a7bd0
JRojo <javier.rojo.moreno@gmail.com>
2020-12-29 10:40:44 +0100

    Quad using Element buffer array

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -89,8 +89,8 @@ int main(void)
 
 	glfwInit();
 
-	//Set version of  OpenGL. Default is last supported by GPU
-	//Also define Core profile (3.2. and above) vs inmediate mode
+	// Set version of  OpenGL. Default is last supported by GPU
+	// Also define Core profile (3.2. and above) vs inmediate mode
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -99,7 +99,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-	//Create window and set context
+	// Create window and set context
 	GLFWwindow* window = glfwCreateWindow(height, width, "OpenGL playground", NULL, NULL);
 	if (window == NULL)
 	{
@@ -110,29 +110,36 @@ int main(void)
 
 	glfwMakeContextCurrent(window);
 
-	//Initialize GLAD and load all OpenGL function pointers. Always do it before call any OpenGL function!!!
+	// Initialize GLAD and load all OpenGL function pointers. Always do it before call any OpenGL function!!!
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	//Print which gl version we're using
+	// Print which gl version we're using
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	//Set OpenGL viewport. This will be where OpenGL renders with respect to windows. First point is lower left corner
+	// Set OpenGL viewport. This will be where OpenGL renders with respect to windows. First point is lower left corner
 	glViewport(0, 0, height, width);
 
-	//Callback for window resize. OnResize we update glViewport
+	// Callback for window resize. OnResize we update glViewport
 	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 
+
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
 	};
 
-	//VBO: 
+	unsigned int indices[] = {
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	// VBO: 
 	// 1.Create 1 buffer and store ID in VBO. This is a reference to an OpenGL Object 
 	// 2.Binding VBO to array buffer
 	// 3.Copy data to Array buffer which affect to VBO as is the object bound to this buffer
@@ -141,43 +148,58 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//VAO: Create and bind
+	// VAO: Create and bind
 	// IMPORTANT!!! VAO must be bind before setting Vertex attibutes, as "linking" occurs between VAO and attributes not between VBO and VAO
 	// Keep in mind that each attribute "links" with data in  VBO 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
-	//Set Vertex Attributes. 
-	//In this case we configure position information and set in layout position 0. It must match which shader
+	// EBO
+	// Important!!! EBO "links" to VAO if VAO is binding and we unbind the EBO, the VAO Wont have and EBO configured and it cause error when drawing indices
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	// Set Vertex Attributes. 
+	// In this case we configure position information and set in layout position 0. It must match which shader
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	//Unbind Array buffer as information persist in VAO
+	// Unbind Array buffer as information persist in VAO
+	// Note that VAO it´s unbind before EBO in order to keep correctly configured
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	//Generate shader
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
+	// Generate shader
 	unsigned int shader = CreateShaderProgram();
-	//Binding shader to use when draw call 
+	// Binding shader to use when draw call 
 	glUseProgram(shader);
 
-	//Game loop
+	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
-		//Input handling
+		// Input handling
 		ProcessInput(window);
 
-		//Rendering stuff
+		// Rendering stuff
 		glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//Draw call
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Wireframe mode 
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-		//Double buffer
+		// Draw call
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// Double buffer
 		glfwSwapBuffers(window);
 
-		//Pooling for input events
+		// Pooling for input events
 		glfwPollEvents();
 
 	}
